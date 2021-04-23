83d540ff9f80cb896e9f7f0a4701c2e1bdde5ea6
Dramchev <dramchev@protonmail.ch>
2021-04-23 01:20:35 +0100

    Add iterations finder, default is 10 if {repeat_times_} omitted.

--- sequence.py
+++ sequence.py
@@ -4,6 +4,8 @@ import re
 
 
 MAX_CHARS = 10
+MAX_ITERATION_CHARS = 3
+DEFAULT_ITERATIONS = 10
 print()
 
 
@@ -28,8 +30,7 @@ def get_number(text):
 
 input_file_name = "input.txt"
 # full incrementer syntax is {increment_from_X}
-# where X is an n<=MAX_CHARS-character number
-incrementer_syntax_start = "{increment_from_"
+# where X is an n <= MAX_CHARS - character number
 
 if not os.path.isfile(input_file_name):
     print("Input file doesn't exist. Create file 'input.txt' "
@@ -38,17 +39,32 @@ else:
     with open('input.txt', 'r') as input_file:
         input_text = input_file.read()
 
+iterations_syntax_start = "{repeat_times_"
+incrementer_syntax_start = "{increment_from_"
+
+indexes = re.finditer(iterations_syntax_start, input_text)
+
+
+iterations = DEFAULT_ITERATIONS
+for index in indexes:
+    match_end = index.span()[1]
+    iterations = get_number(
+        input_text[match_end:match_end + MAX_ITERATION_CHARS])
+
 indexes = re.finditer(incrementer_syntax_start, input_text)
 
 start_values = []
 
-for x in indexes:
-    match_end = x.span()[1]
+for index in indexes:
+    match_end = index.span()[1]
     start_values.append(
         get_number(input_text[match_end:match_end + MAX_CHARS]))
 
 # cut main string into list of strings, using the indexes
 # (calculate from that and numbers string list for end of token
 
+for iteration in range(iterations):
+    for index in indexes:
+        span
 
 # create string from cut up main string + list of numbers to int
